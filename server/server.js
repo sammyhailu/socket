6f8908f7197597b808603af35cd2dbe048d9e855
bonjour <sammyhailu56@gmail.com>
2021-06-11 23:14:49 +0300

    setup socket server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+
 
 const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
